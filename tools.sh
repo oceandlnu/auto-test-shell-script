c4003308adf36d0c08d8355854b7ed9a49ad28a5
oceandlnu <136494666@qq.com>
2017-11-22 21:20:51 +0800

    update

--- tools.sh
+++ tools.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 
-. /home/ocean/Documents/code/sh_code/devices_list.sh
-. /home/ocean/Documents/code/sh_code/install_apk.sh
-. /home/ocean/Documents/code/sh_code/pull_log.sh
-. /home/ocean/Documents/code/sh_code/local_monkey.sh
-. /home/ocean/Documents/code/sh_code/offline_monkey.sh
+path=/home/ocean/Documents/code/sh_code/
+
+. ${path}devices_list.sh
+. ${path}install_apk.sh
+. ${path}pull_log.sh
+. ${path}local_monkey.sh
+. ${path}offline_monkey.sh
 
 echo "---------------------------"
 echo "1-安装当前目录apk文件到所有设备"
 echo "2-拉取所有设备日志到本地"
 echo "3-多设备本地化monkey"
 echo "4-多设备离线monkey"
+echo "<CTRL+D>退出"
 echo "---------------------------"
 
 #调用devicesList函数
 devicesList
 
-read sNum
-
-case $sNum in
-    1 )
-        installAPK
-        ;;
-    2 )
-        pullLog
-        ;;
-    3 )
-        localMonkey
-        ;;
-    4 )
-        offlineMonkey
-        ;;
-    * )
-        echo "error"
-        ;;
-esac
+while read sNum
+do
+    case $sNum in
+        1 )
+            installAPK
+            ;;
+        2 )
+            pullLog
+            ;;
+        3 )
+            localMonkey
+            ;;
+        4 )
+            offlineMonkey
+            ;;
+        * )
+            echo "输入错误，请重新输入，或按<CTRL+D>退出"
+            ;;
+    esac
+    echo "---------------------------"
+    echo "1-安装当前目录apk文件到所有设备"
+    echo "2-拉取所有设备日志到本地"
+    echo "3-多设备本地化monkey"
+    echo "4-多设备离线monkey"
+    echo "<CTRL-D> 退出"
+    echo "---------------------------"
+done
 
 # echo ${arr_num[*]}

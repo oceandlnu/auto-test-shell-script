022982c0376c9a8ab597f21e835ae44caae62b89
oceandlnu <136494666@qq.com>
2017-12-21 10:00:01 +0800

    update

--- devices_list.sh
+++ devices_list.sh
@@ -11,15 +11,19 @@ function devicesList() {
     i=0
 
     for index in ${devices_num}; do
-        if [[ `echo "${index}" | grep -c "nopermissions*"` == 1 ]]; then
+        if [[ `echo "${index}" | grep -c "unauthorized"` == 1 ]]; then
+            echo "设备${index%unauthorized*}未允许本机USB调试"
+        elif [[ `echo "${index}" | grep -c "nopermissions*"` == 1 ]]; then
             # echo "设备`echo "${index}" | sed 's/nopermissions*//g'`未允许本机调试"
-            echo "设备${index%nopermissions*}未允许本机调试"
+            echo "请将设备\"${index%nopermissions*}\"USB使用方式选择为\"传输文件\""
         elif [[ `echo "${index}" | grep -c "device"` == 1 ]]; then
             arr_num[$i]=`echo "${index}" | sed 's/device//g'`
             device_temp_name=`adb -s ${arr_num[$i]} shell getprop ro.product.model`_`adb -s ${arr_num[$i]} shell getprop ro.build.version.release`
             device_name=`echo "${device_temp_name}" | sed 's/\s//g'`
             echo "${device_name}"
             let "i++"
+        else
+            echo "设备未开启USB调试选项"
         fi
     done
 
